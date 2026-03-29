db314d9a779c79c6d620ea4f5776aa383d538c93
Menna Elshahawy <mennatullah.elshahawy@gmail.com>
2026-03-29 13:50:17 +0200

    Added Top 5 Memory and CPU usage processes

--- Server_Performance_Stats/server-stats.sh
+++ Server_Performance_Stats/server-stats.sh
@@ -15,3 +15,12 @@ free | grep Mem | awk '{print $4/$2*100.0}'
 echo ""
 echo "Disk Usage:"
 df -h
+
+echo ""
+echo "Top 5 CPU usage processes"
+ps aux --sort=-%cpu | head -n6
+
+echo ""
+echo "Top 5 Memory Usage processes"
+ps aux --sort=-%mem | head -n6
+
